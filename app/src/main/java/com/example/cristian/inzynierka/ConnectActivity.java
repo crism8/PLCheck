647ee8c8bf158aba46fcbaf57bdbbf057b9e88de
crism8 <cmakarski@gmail.com>
2018-01-08 23:56:49 +0100

    ConnectionUI

--- app/src/main/java/com/example/cristian/inzynierka/ConnectActivity.java
+++ app/src/main/java/com/example/cristian/inzynierka/ConnectActivity.java
@@ -1,84 +1,107 @@
 package com.example.cristian.inzynierka;
 
 import android.app.Activity;
-import android.app.VoiceInteractor;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import Moka7.*;
 
 public class ConnectActivity extends AppCompatActivity {
-
     String ipAd;
     int slotInt;
     int rackInt;
     S7Client client = new S7Client();
+    EditText ipAddress;
+    EditText slot;
+    EditText rack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        setupUI();
+
     }
-   // S7Client client = new S7Client();
+    public void setupUI() {
+        EditText ipAddress = (EditText) findViewById(R.id.addressIPEditText);
+        EditText slot = (EditText) findViewById(R.id.slotEditText);
+        EditText rack = (EditText) findViewById(R.id.rackEditText);
+        ipAddress.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+        slot.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+        rack.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+    }
 
     public void plcConnect (View view) {
         EditText ipAddress = (EditText) findViewById(R.id.addressIPEditText);
         EditText slot = (EditText) findViewById(R.id.slotEditText);
         EditText rack = (EditText) findViewById(R.id.rackEditText);
-
-        ipAddress.setText("192.168.1.100"); //ipAddress.getText().toString();
-        slot.setText("1"); //Integer.parseInt(slot.getText().toString());
-        rack.setText("0"); //Integer.parseInt(rack.getText().toString());
+        slotInt = Integer.parseInt(slot.getText().toString());
+        rackInt = Integer.parseInt(rack.getText().toString());
         ipAd = ipAddress.getText().toString();
         Log.d("myTag", "This is my message:"+ ipAd);
-        new PlcReader().execute("");
+        new PLCConnectionChecker().execute("");
     }
 
-    public void connectionTest (View view) {
-        client.SetConnectionType(S7.S7_BASIC);
-        int res = client.ConnectTo("192.168.1.100", 0,1);
-        Log.d("myTag", "This is my message"+ res);
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
-
-    private class PlcReader extends AsyncTask<String, Void, String> {
-    String ret = "";
-
+    private class PLCConnectionChecker extends AsyncTask<String, Void, String> {
+        String ret = "";
         @Override
         protected String doInBackground(String... params) {
             try{
                 client.SetConnectionType(S7.S7_BASIC);
-                int res = client.ConnectTo(ipAd, 0,1);
+                int res = client.ConnectTo(ipAd, rackInt,slotInt);
 
                 if (res==0){ //connection is ok
-                        byte[] data = new byte[4];
-                    res = client.ReadArea(S7.S7AreaDB,7,0,4,data);
-                    ret = "value of DB7.DBD0:"+S7.GetFloatAt(data,0);
-            }else{
+                    //byte[] data = new byte[4];
+                    //res = client.ReadArea(S7.S7AreaDB,7,0,4,data);
+                    //ret = "value of DB7.DBD0:"+S7.GetFloatAt(data,0);
+                    ret = "Connection established.";
+                }else{
                     ret= "ERR: "+ S7Client.ErrorText(res);
                 }
                 client.Disconnect();
-        }catch (Exception e) {
+            }catch (Exception e) {
                 ret = "EXC: " +e.toString();
                 Thread.interrupted();
             }
             return "executed";
-    }
+        }
 
-    @Override
-    protected void onPostExecute(String result) {
-        TextView txout = (TextView) findViewById(R.id.ConnectTextView);
-        txout.setText(ret);
-
-    }
+        @Override
+        protected void onPostExecute(String result) {
+            TextView txout = (TextView) findViewById(R.id.ConnectTextView);
+            txout.setText(ret);
+        }
     }
 }
